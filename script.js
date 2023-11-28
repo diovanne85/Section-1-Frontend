6a4498e5c1481988d848584326f2e0f9e1fbc7e6
diovanne85 <dpsagosoy85@gmail.com>
2023-11-29 01:15:41 +0800

    working

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let progress = document.getElementById("progress");
+  let song = document.getElementById("song");
+  let ctrlIcon = document.getElementById("playBtn");
+  let timerStart = document.getElementById("timer-start");
+  let timerEnd = document.getElementById("timer-end");
+
+  song.onloadedmetadata = function () {
+    const durationMinutes = Math.floor(song.duration / 60);
+    const durationSeconds = Math.floor(song.duration % 60);
+    timerEnd.textContent = `${durationMinutes}:${durationSeconds}`;
+  };
+
+  function playPause() {
+    if (ctrlIcon.classList.contains("fa-pause")) {
+      song.pause();
+      ctrlIcon.classList.remove("fa-pause");
+      ctrlIcon.classList.add("fa-play");
+    } else {
+      song.play();
+      ctrlIcon.classList.add("fa-pause");
+      ctrlIcon.classList.remove("fa-play");
+    }
+  }
+
+  song.addEventListener("timeupdate", function () {
+    const currentTimeMinutes = Math.floor(song.currentTime / 60);
+    const currentTimeSeconds = Math.floor(song.currentTime % 60);
+    timerStart.textContent = `${currentTimeMinutes}:${currentTimeSeconds}`;
+
+    progress.value = song.currentTime;
+  });
+
+  ctrlIcon.addEventListener("click", playPause);
+});
